dfb07d6a1a1bcb47a20c7a2b11f8a5b0bba57112
Kzim <kazemaraby@gmail.com>
2021-10-25 16:15:31 +0200

    delete from modules  files

--- dynamic/install.py
+++ dynamic/install.py
@@ -1,5 +1,5 @@
 import frappe
-
+import os
 def after_install():
 	print("+dynamic")
 	try:
@@ -13,5 +13,18 @@ def after_install():
 		frappe.db.commit()
 		frappe.db.sql("""delete from `tabModule Def` where name='Payroll'""")
 		frappe.db.commit()
-	except:
+
+		print("+del module.text")
+		#### after that del them from module
+		a_file = open("../apps/erpnext/erpnext/modules.txt", "r")
+		lines = a_file.readlines()
+		a_file.close()
+		new_file = open("../apps/erpnext/erpnext/modules.txt", "w")
+		for line in lines:
+			print("lines",line)
+			if line.strip("\n") not in ("Loan Management","Payroll"):
+				new_file.write(line)
+		new_file.close()
+	except Exception as e:
+		print(e)
 		pass
