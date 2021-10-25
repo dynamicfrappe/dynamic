3004ae28697021cc8de5510e0e9cc58762e4092e
Kzim <kazemaraby@gmail.com>
2021-10-25 15:09:11 +0200

    del payroll and loan

--- dynamic/install.py
+++ dynamic/install.py
@@ -3,6 +3,10 @@ import frappe
 def after_install():
 	try:
 		frappe.db.sql("""delete from tabWorkspace where name in ("HR","Loans","Payroll")""")
+		frappe.db.sql("""delete from tabDocType where module='Loan Management'""")
+		frappe.db.sql("""delete from `tabModule Def` where name='Loan Management'""")
+		frappe.db.sql("""delete from tabDocType where module='Payroll'""")
+		frappe.db.sql("""delete from `tabModule Def` where name='Payroll'""")
 		frappe.db.commit()
 	except:
 		pass
