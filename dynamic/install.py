d72658d909fa877c689efd9958c90441d237a90d
Kzim <kazemaraby@gmail.com>
2021-10-25 15:24:10 +0200

    edit

--- dynamic/install.py
+++ dynamic/install.py
@@ -1,11 +1,16 @@
 import frappe
 
 def after_install():
+	print("+dynamic")
 	try:
 		frappe.db.sql("""delete from tabWorkspace where name in ("HR","Loans","Payroll")""")
+		frappe.db.commit()
 		frappe.db.sql("""delete from tabDocType where module='Loan Management'""")
+		frappe.db.commit()
 		frappe.db.sql("""delete from `tabModule Def` where name='Loan Management'""")
+		frappe.db.commit()
 		frappe.db.sql("""delete from tabDocType where module='Payroll'""")
+		frappe.db.commit()
 		frappe.db.sql("""delete from `tabModule Def` where name='Payroll'""")
 		frappe.db.commit()
 	except:
