4e303728001e6a10e07c1ee6a3c889d4c12b3a05
Kzim <kazemaraby@gmail.com>
2021-10-25 13:35:02 +0200

    add

---
+++ dynamic/public/js/dynamic.js
@@ -0,0 +1,19 @@
+localStorage.setItem("container_fullwidth","true")
+obj={
+        "Let's Set Up the Payroll Module. ":"2021-10-25 12:45:48",
+        "Let's Set Up Your Accounts and Taxes.":"2021-10-25 12:50:14",
+        "Let's Set Up the Human Resource Module.":"2021-10-25 12:50:14",
+        "Let's Set Up the Assets Module.":"2021-10-25 12:50:14",
+        "Let's Set Up the Buying Module.":"2021-10-25 12:50:14",
+        "Let's Set Up Your CRM.":"2021-10-25 12:50:14",
+        "Let's Set Up the Education Module.":"2021-10-25 12:50:14",
+        "Let's Set Up the Healthcare Module.":"2021-10-25 12:50:14",
+        "Let's Set Up the Manufacturing Module.":"2021-10-25 12:50:14",
+        "Let's Set Up the Selling Module.":"2021-10-25 12:50:14",
+        "Let's Set Up the Stock Module.":"2021-10-25 12:50:14"
+    }
+
+localStorage.setItem(
+				"dismissed-onboarding",
+				JSON.stringify(obj)
+			);
