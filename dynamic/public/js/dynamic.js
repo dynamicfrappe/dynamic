b0c7ac86a940ada1c28a377f9957c1cb5f853f09
beshoy <beshoyatef31@gmail.com>
2021-11-14 12:06:39 +0200

    local changes

--- dynamic/public/js/dynamic.js
+++ dynamic/public/js/dynamic.js
@@ -1,23 +1,6 @@
-// localStorage.setItem("container_fullwidth","true")
 
 $(window).on("load", function () {
     $(".dropdown-help").attr("style", "display: none !important");
-    // localStorage.setItem("container_fullwidth","true")
-//   frappe.after_ajax(function () {
-//     // $('.dropdown-help').css('display','block');
-//     $(".dropdown-help").attr("style", "display: none !important");
+    localStorage.setItem("container_fullwidth","true")
 
-//     // if (frappe.boot.whitelabel_setting.logo_width) {
-//     //     $('.app-logo').css('width',frappe.boot.whitelabel_setting.logo_width+'px');
-//     // }
-//     // if (frappe.boot.whitelabel_setting.logo_height) {
-//     //     $('.app-logo').css('height',frappe.boot.whitelabel_setting.logo_height+'px');
-//     // }
-//     // if (frappe.boot.whitelabel_setting.navbar_background_color) {
-//     //     $('.navbar').css('background-color',frappe.boot.whitelabel_setting.navbar_background_color)
-//     // }
-//     // if (frappe.boot.whitelabel_setting.custom_navbar_title_style && frappe.boot.whitelabel_setting.custom_navbar_title) {
-//     //     $(`<span style=${frappe.boot.whitelabel_setting.custom_navbar_title_style.replace('\n','')} class="hidden-xs hidden-sm">${frappe.boot.whitelabel_setting.custom_navbar_title}</span>`).insertAfter("#navbar-breadcrumbs")
-//     // }
-//   });
 });
