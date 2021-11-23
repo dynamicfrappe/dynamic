cbf6701a76a6316adf6f4a28daaf8dcde133f6da
Kzim <kazemaraby@gmail.com>
2021-11-23 16:04:20 +0200

    remove install

--- dynamic/install.py
+++ dynamic/install.py
@@ -3,35 +3,35 @@ import os
 import json
 def after_install():
 	print("+dynamic")
-	try:
-		frappe.db.sql("""delete from tabWorkspace where name in ("HR","Loans","Payroll","Quality","Projects","Support")""")
-		frappe.db.commit()
-		frappe.db.sql("""delete from tabDocType where module='Loan Management'""")
-		frappe.db.commit()
-		frappe.db.sql("""delete from `tabModule Def` where name='Loan Management'""")
-		frappe.db.commit()
-		# frappe.db.sql("""delete from tabDocType where module='Payroll'""")
-		# frappe.db.commit()
-		frappe.db.sql("""delete from tabDocType where module="Payroll" and name!='Salary Component'""")
-		frappe.db.commit()
-		# frappe.db.sql("""delete from tabDocType where module='Projects'""")
-		# frappe.db.commit()
-		# frappe.db.sql("""delete from `tabModule Def` where name='Projects'""")
-		# frappe.db.commit()
-		print("+del module.text")
-		#### after that del them from module
-		a_file = open("../apps/erpnext/erpnext/modules.txt", "r")
-		lines = a_file.readlines()
-		a_file.close()
-		new_file = open("../apps/erpnext/erpnext/modules.txt", "w")
-		for line in lines:
-			if line.strip("\n") !="Loan Management":
-				new_file.write(line)
-		new_file.close()
-		install_uom()
-	except Exception as e:
-		print(e)
-		pass
+	# try:
+	# 	frappe.db.sql("""delete from tabWorkspace where name in ("HR","Loans","Payroll","Quality","Projects","Support")""")
+	# 	frappe.db.commit()
+	# 	frappe.db.sql("""delete from tabDocType where module='Loan Management'""")
+	# 	frappe.db.commit()
+	# 	frappe.db.sql("""delete from `tabModule Def` where name='Loan Management'""")
+	# 	frappe.db.commit()
+	# 	# frappe.db.sql("""delete from tabDocType where module='Payroll'""")
+	# 	# frappe.db.commit()
+	# 	frappe.db.sql("""delete from tabDocType where module="Payroll" and name!='Salary Component'""")
+	# 	frappe.db.commit()
+	# 	# frappe.db.sql("""delete from tabDocType where module='Projects'""")
+	# 	# frappe.db.commit()
+	# 	# frappe.db.sql("""delete from `tabModule Def` where name='Projects'""")
+	# 	# frappe.db.commit()
+	# 	print("+del module.text")
+	# 	#### after that del them from module
+	# 	a_file = open("../apps/erpnext/erpnext/modules.txt", "r")
+	# 	lines = a_file.readlines()
+	# 	a_file.close()
+	# 	new_file = open("../apps/erpnext/erpnext/modules.txt", "w")
+	# 	for line in lines:
+	# 		if line.strip("\n") !="Loan Management":
+	# 			new_file.write(line)
+	# 	new_file.close()
+	# 	install_uom()
+	# except Exception as e:
+	# 	print(e)
+	# 	pass
 
 
 def install_uom():
