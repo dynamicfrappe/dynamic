8bb1ac52cd04fe006654d5151def29c4fb5e7c33
Kzim <kazemaraby@gmail.com>
2021-12-01 16:31:33 +0200

    comparison

--- dynamic/contracting/doctype/comparison/comparison.py
+++ dynamic/contracting/doctype/comparison/comparison.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2021, Dynamic and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Comparison(Document):
 	pass
+@frappe.whitelist()
+def get_item_price(item_code):
+	try :
+		if item_code:
+			price_list = frappe.db.sql(f"""select * from `tabItem Price` where item_code='{item_code}' and selling=1""",as_dict=1)
+			print("price_list",price_list)
+			if len(price_list) > 0:
+				return price_list[0].price_list_rate
+			return 0
+	except:
+		pass
