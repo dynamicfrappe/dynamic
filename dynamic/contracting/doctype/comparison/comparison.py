c4d304e2b198ca2b249d7a367c8f1da4592cb678
Kzim <kazemaraby@gmail.com>
2021-12-05 15:48:22 +0200

    item cart

--- dynamic/contracting/doctype/comparison/comparison.py
+++ dynamic/contracting/doctype/comparison/comparison.py
@@ -4,8 +4,20 @@
 import frappe
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
+import json
 class Comparison(Document):
-	pass
+	@frappe.whitelist()
+	def get_items(self, for_raw_material_request=0):
+		items = []
+		for i in self.item:
+			items.append(dict(
+				name=i.name,
+				item_code=i.clearance_item,
+				qty=i.qty,
+				price=i.price,
+				total=i.total_price
+			))
+		return items
 @frappe.whitelist()
 def get_item_price(item_code):
 	try :
@@ -58,3 +70,15 @@ def make_sales_order(source_name, target_doc=None, ignore_permissions=False):
 	}, target_doc,postprocess, ignore_permissions=ignore_permissions)
 
 	return doclist
+@frappe.whitelist()
+def create_item_cart(items,comparison,tender=None):
+	items = json.loads(items).get('items')
+	print("from ifffffffffffff",items)
+	print("comparison",comparison)
+	for item in items:
+		doc = frappe.new_doc("Comparison Item Card")
+		doc.item_code  = item.get("item_code")
+		doc.comparison = comparison
+		doc.tender	   = tender
+		doc.save()
+	return True
