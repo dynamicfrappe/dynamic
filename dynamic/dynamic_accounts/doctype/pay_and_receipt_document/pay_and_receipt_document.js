45506097c940dc89f8a62788b7e66ebe781338bd
EngPeterMaged <eng.peter.maged@gmail.com>
2022-03-21 11:33:47 +0200

    add pay receive document child

--- dynamic/dynamic_accounts/doctype/pay_and_receipt_document/pay_and_receipt_document.js
+++ dynamic/dynamic_accounts/doctype/pay_and_receipt_document/pay_and_receipt_document.js
@@ -13,6 +13,7 @@ frappe.ui.form.on("Pay and Receipt Document", {
         },
       };
     });
+    
     frm.set_query("against_account", function (doc) {
       return {
         filters: {
@@ -21,6 +22,31 @@ frappe.ui.form.on("Pay and Receipt Document", {
           company: doc.company,
         },
       };
+    });
+    frm.set_query("account","accounts", function (doc) {
+      return {
+        filters: {
+          is_group: 0,
+          // "account_type": doc.mode_of_payment ,
+          company: doc.company,
+        },
+      };
+    });
+		frm.set_query("party_type","accounts", function() {
+			return {
+				query: "erpnext.setup.doctype.party_type.party_type.get_party_type",
+			};
+		});
+  },
+  set_totals:function(frm){
+    frappe.call({
+      method: "set_totals",
+      doc: frm.doc,
+      callback: function (r) {
+        frm.refresh_field("total");
+        frm.refresh_field("difference");
+        frm.refresh_field("accounts");
+      },
     });
   },
   currency(frm) {
@@ -31,15 +57,18 @@ frappe.ui.form.on("Pay and Receipt Document", {
         callback: function (r) {
           frm.events.set_base_amount(frm);
           frm.refresh_field("exchange_rate");
+          frm.events.set_totals(frm);
         },
       });
     }
   },
   amount(frm) {
     frm.events.set_base_amount(frm);
+    frm.events.set_totals(frm);
   },
   exchange_rate(frm) {
     frm.events.set_base_amount(frm);
+    frm.events.set_totals(frm);
   },
   set_base_amount(frm) {
     frm.doc.base_amount = 0;
@@ -49,3 +78,12 @@ frappe.ui.form.on("Pay and Receipt Document", {
     frm.refresh_field("base_amount");
   },
 });
+
+frappe.ui.form.on("Pay and Receipt Account", {
+  accounts_add:function(frm,cdt,cdn){
+    frm.events.set_totals(frm)
+  },
+  amount:function(frm,cdt,cdn){
+    frm.events.set_totals(frm)
+  }
+})
