b6f35e5474fd87e8077b3b1e568c2d9c077b4fff
Kzim <kazemaraby@gmail.com>
2022-03-21 17:00:20 +0200

    update

--- dynamic/domains/gebco.py
+++ dynamic/domains/gebco.py
@@ -3,22 +3,14 @@ from __future__ import unicode_literals
 data = {
 
     'custom_fields': {
-        'Item': [
-            {
-                "fieldname": "serial1",
-                "fieldtype": "Data",
-                "insert_after": "is_item_from_hub",
-                "label": "Serial 1",
-                "unique": 1,
-                "reqd": 1,
-            },
+        'Serial No': [
             {
                 "fieldname": "serial2",
                 "fieldtype": "Data",
-                "insert_after": "serial1",
-                "label": "Serial 2",
+                "insert_after": "serial_no",
+                "label": "Serial No 2",
                 "unique": 1,
-            }
+            },
         ]
     },
     "properties": [
