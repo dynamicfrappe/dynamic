cffccc2c8e3acfb6ec0217068ed07616fad3ba13
Kzim <kazemaraby@gmail.com>
2022-03-21 15:10:17 +0200

    renew the contract

--- dynamic/gebco/doctype/maintenance_contract/maintenance_contract.js
+++ dynamic/gebco/doctype/maintenance_contract/maintenance_contract.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Maintenance Contract', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        if (frm.doc.docstatus == 0) {
+            console.log("from if")
+            frm.add_custom_button(__("Renew"), function() {
+                //console.log("renew")
+                frappe.model.open_mapped_doc({
+                    method: "dynamic.gebco.doctype.maintenance_contract.maintenance_contract.renew_contract",
+                    frm: frm,
+                });
+            });
+        }
+    }
 });
--- dynamic/gebco/doctype/maintenance_contract/maintenance_contract.py
+++ dynamic/gebco/doctype/maintenance_contract/maintenance_contract.py
@@ -1,8 +1,31 @@
 # Copyright (c) 2022, Dynamic and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MaintenanceContract(Document):
 	pass
+
+@frappe.whitelist()
+def renew_contract(source_name, target_doc=None):
+	doc = frappe.get_doc("Maintenance Contract", source_name)
+	new_contract = frappe.new_doc("Maintenance Contract")
+	new_contract.from_date  = doc.from_date
+	new_contract.visits		= doc.visits
+	new_contract.to_date 	= doc.to_date
+	new_contract.number_of_visits = doc.number_of_visits
+	new_contract.customer   	  = doc.customer
+	new_contract.customer_name 	  = doc.customer_name
+	new_contract.contract_value   = doc.contract_value
+	new_contract.guarantee 		  = doc.guarantee
+	new_contract.number_of_cars   = doc.number_of_cars
+	for pl in doc.cars_plate_numbers:
+		new_contract.append('cars_plate_numbers',{
+			"plate_number":pl.plate_number
+		})
+	#new_contract.save()
+	return new_contract
+
+
+
