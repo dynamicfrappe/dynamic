3b5e30591a0268199c6e237198bda6173380ac2f
Kzim <kazemaraby@gmail.com>
2022-03-22 17:18:55 +0200

    gebco

--- dynamic/gebco/doctype/maintenance_template/maintenance_template.js
+++ dynamic/gebco/doctype/maintenance_template/maintenance_template.js
@@ -3,9 +3,17 @@
 
 frappe.ui.form.on('Maintenance Template', {
     refresh: function(frm) {
-        // if(frm.doc.__islocal ==0){
-        //     if(frm.doc.)
-        // }
+        if (frm.doc.__islocal == 0) {
+            if (frm.doc.include_spare_part == "YES") {
+                frm.add_custom_button(__("Create Stock Entry"), function() {
+
+                });
+            } else {
+                frm.add_custom_button(__("Create Delivery Note"), function() {
+
+                });
+            }
+        }
     }
 });
 
@@ -22,6 +30,26 @@ frappe.ui.form.on('Cars Plate Numbers For Template', {
                     if (count > 1) {
                         row.plate_number = ""
                         frappe.msgprint("This Plate Number Already Exist")
+                    }
+                }
+            }
+        }
+    }
+})
+
+frappe.ui.form.on('Template Cars', {
+    car: (frm, cdt, cdn) => {
+        let row = locals[cdt][cdn]
+            //console.log(row.plate_number)
+        if (row.car.length > 1) {
+            let count = 0
+            for (let i = 0; i < frm.doc.cars.length; i++) {
+                if (frm.doc.cars[i].car == row.car) {
+
+                    count += 1
+                    if (count > 1) {
+                        row.car = ""
+                        frappe.msgprint("This Car Already Exist")
                     }
                 }
             }
