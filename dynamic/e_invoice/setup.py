8c89a0ab30cbe6864ec7acb448516e89b373f6d3
abanoub_mounir <abanoubmounir07@gmail.com>
2022-06-07 15:08:15 +0200

    aut_add_tax_types

--- dynamic/e_invoice/setup.py
+++ dynamic/e_invoice/setup.py
@@ -1,3 +1,9 @@
+from email import message
+from pydoc import importfile
+# from tkinter import E
+import json
+from pathlib import Path
+import os
 import frappe
 import os
 import json
@@ -19,6 +25,10 @@ def install_e_invoice():
 		# frappe.msgprint(str(e))
 	try:
 		sales_invoice_script()
+	except:
+		pass
+	try:
+		import_tax_types()
 	except:
 		pass
 
@@ -101,6 +111,39 @@ def sales_invoice_script():
 
 
 
+path_file = "dynamic/dynamic/public/tax_types/"
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+
+path_fiels = ["sinv1.json","sinv2.json","sinv3.json"]
+@frappe.whitelist()
+def import_tax_types():
+	try :
+		for fname in path_fiels:
+			file_path = path_file + fname
+			file_path =  os.path.join(BASE_DIR,file_path)
+			main_file = open(file_path)
+			data_js = json.load(main_file)
+			insert_data(data_js)
+	except Exception as e:
+		error = frappe.new_doc("Error Log")
+		error.error = str(e) #str(e)[0:200] if len(e) > 200 else e
+		error.save() 
+
+def insert_data(data_js):
+	try :
+		for  data in data_js:
+			tax_type = frappe.new_doc("Tax Types")
+			tax_type.code = data["Code"] if data["Code"] else ''
+			tax_type.desc_en = data["Desc_en"] if data["Desc_en"] else ''
+			tax_type.desc_ar = data["Desc_ar"] if data["Desc_ar"] else ''
+			tax_type.taxtypereference = data.get("TaxtypeReference","")
+			tax_type.save()
+		return 'done'
+
+	except Exception as e:
+		error = frappe.new_doc("Error Log")
+		error.error = str(e) #str(e)[0:200] if len(e) > 200 else e
+		error.save() 
 
 
 
