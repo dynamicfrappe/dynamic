4b51114b8361886d24c7b68c7e9ab59876efccc1
EngPeterMaged <eng.peter.maged@gmail.com>
2022-06-16 09:37:10 +0200

    serial car query

--- dynamic/gebco/doctype/car/car.py
+++ dynamic/gebco/doctype/car/car.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2022, Dynamic and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Car(Document):
 	pass
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_serial_no_query(doctype, txt, searchfield, start, page_len, filters):
+	docname = filters.get("docname") or ""
+	return frappe.db.sql(f"""
+	select name,item_code from `tabSerial No` where name not in (
+		select serial_no from tabCar where docstatus < 2
+		and name <> '{docname}'
+ 		) and( name like '%{txt}%' or item_code like '%{txt}%' )
+		limit {start} , {page_len}""")
