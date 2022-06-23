7c5df1b1fbc484b79af52d4effeab11753c5c1d6
Kazem <kazemaraby@gmail.com>
2022-06-23 12:31:51 +0200

    update

--- dynamic/terra/report/actions_report/actions_report.js
+++ dynamic/terra/report/actions_report/actions_report.js
@@ -5,6 +5,39 @@
 frappe.query_reports["Actions Report"] = {
 	// asd
 	"filters": [
-
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date"
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date"
+		},
+		{
+			fieldname: "type",
+			label: __("Type"),
+			fieldtype: "Select",
+			options:"\nIndoor\nOut door"
+		},
+		{
+			fieldname: "actions",
+			label: __("Action"),
+			fieldtype: "Link",
+			options:"Action"
+		},
+		{
+			fieldname: "customer_type",
+			label: __("Customer Type"),
+			fieldtype: "Select",
+			options:"\nLead\nOpportunity"
+		},
+		{
+			fieldname: "branch",
+			label: __("Branch"),
+			fieldtype: "Link",
+			options:"Branch"
+		}
 	]
 };
