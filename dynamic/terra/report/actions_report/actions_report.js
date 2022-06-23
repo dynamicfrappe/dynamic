4fc82e34305d141a026b54f3ee5357f1a7f79b36
Kazem <kazemaraby@gmail.com>
2022-06-23 11:33:56 +0200

    action report and stock setting

---
+++ dynamic/terra/report/actions_report/actions_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Dynamic and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Actions Report"] = {
+	"filters": [
+
+	]
+};
