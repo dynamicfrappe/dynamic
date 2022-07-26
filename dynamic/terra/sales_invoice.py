0f35c9cc539692ac9aaf62795ccd558ec9ace6ea
beshoy <beshoyatef31@gmail.com>
2022-07-26 13:49:46 +0200

    update reservation

--- dynamic/terra/sales_invoice.py
+++ dynamic/terra/sales_invoice.py
@@ -10,3 +10,14 @@ def check_return_account(doc):
             for item in doc.items:
                 if item.get("default_income_account") != company.get("sales_return_account"):
                     frappe.msgprint("Default Income Account Changed in Line %s"%item.idx)
+
+
+def validate_sales_invoices(doc):
+
+    # validate sales invocie linked with sales order 
+    for line in doc.items :
+        if not line.sales_order : 
+            frappe.throw(_(f"""You can not add Sales Invocie withou Sales Order 
+                                Please Check item {line.item_name}"""))
+    if doc.update_stock ==1  : 
+        frappe.throw("validate")
