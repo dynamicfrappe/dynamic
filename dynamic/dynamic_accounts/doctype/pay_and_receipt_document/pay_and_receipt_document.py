50136152a01c6f0750bdd8537ea7a47f96ea440f
Peter Maged <eng.peter.maged@gmail.com>
2022-08-14 16:55:16 +0200

    against accout

--- dynamic/dynamic_accounts/doctype/pay_and_receipt_document/pay_and_receipt_document.py
+++ dynamic/dynamic_accounts/doctype/pay_and_receipt_document/pay_and_receipt_document.py
@@ -114,6 +114,7 @@ class PayandReceiptDocument(Document):
 					"exchange_rate": flt(account_row_exchange_rate),
 					"credit_in_account_currency": flt(amount_in_account_row_currency),
 					"credit_in_company_currency": flt(account_row.base_amount),
+                	"against_account": self.account,
 					"cost_center": self.cost_center,
 					"project": self.project,
 					"user_remark": getattr(account_row, "note", (self.notes or "")),
@@ -157,6 +158,7 @@ class PayandReceiptDocument(Document):
 				je.append("accounts", {
 					"account": account_row.account,
 					"account_currency": account_row_currency,
+                	"against_account": self.account,
 					"exchange_rate": flt(account_row_exchange_rate),
 					"debit_in_account_currency":  flt(amount_in_account_row_currency),
 					"debit_in_company_currency": flt(account_row.base_amount),
