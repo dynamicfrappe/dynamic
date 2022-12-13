c50156da8a0f1ab4b641feb2cdd29529b1c2cd60
beshoy <beshoyatef31@gmail.com>
2022-12-13 12:03:50 +0200

    add local

--- dynamic/public/dashboard/sales_order_dashboard.py
+++ dynamic/public/dashboard/sales_order_dashboard.py
@@ -1,17 +1,13 @@
 
 from frappe import _
 from frappe import get_active_domains
+#/home/beshoy/Dynamic-13/tera/frappe-tera/apps/erpnext/erpnext/selling/doctype/sales_order/sales_order_dashboard.py
+from erpnext.selling.doctype.sales_order.sales_order_dashboard import get_data as dashboard_data_data
 DOMAINS = get_active_domains()
 
 
 def get_data(data={}):
-    dashboard_data = {
-        'fieldname': 'sales_order',
-        'non_standard_fieldnames': {
-            'Journal Entry': 'reference_name',
-        },
-        "transactions": []
-    }
+    dashboard_data = dashboard_data_data()
     if "Cheques" in DOMAINS:
         dashboard_data["transactions"].append(
             {
