a6223f91c702206dc582cc3b7e4242512821c390
Peter Maged <eng.peter.maged@gmail.com>
2022-12-21 16:27:20 +0200

    add payment table in create sales order from qquotation

--- dynamic/ifi/api.py
+++ dynamic/ifi/api.py
@@ -145,6 +145,9 @@ def _make_sales_order(source_name, target_doc=None, ignore_permissions=False):
 		target.flags.ignore_permissions = ignore_permissions
 		target.run_method("set_missing_values")
 		target.run_method("calculate_taxes_and_totals")
+		#Get the advance paid Journal Entries in Sales Invoice Advance
+		if target.get("allocate_advances_automatically"):
+			target.set_advances()
 
 	def update_item(obj, target, source_parent):
 		target.stock_qty = flt(obj.qty) * flt(obj.conversion_factor)
@@ -163,6 +166,7 @@ def _make_sales_order(source_name, target_doc=None, ignore_permissions=False):
                 "field_map": {
                     "crean": "crean",
                     "crean_amount": "crean_amount",
+					"allocate_advances_automatically":"allocate_advances_automatically"
                 },
                 "validation": 
                 {"docstatus": ["=", 1]}
