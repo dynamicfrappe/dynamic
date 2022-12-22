49516c153989bf0fef53cf501945080434ff7797
Kazem <kazemaraby@gmail.com>
2022-12-22 16:08:33 +0200

    vero

--- dynamic/domains/vero.py
+++ dynamic/domains/vero.py
@@ -11,25 +11,6 @@ data = {
             "insert_after":"company_name",
             "label":"Company Arabic Name",
         }
-       ],
-    },
-    # "properties": [
-        
-    # ],
-    "properties": [
-       
-    ],
-#     "property_setters": [
-#     {
-#         "doc_type": "Batch",
-#         "doctype_or_field": "DocType",
-#         "name": "Batch-main-search_fields",
-#         "owner": "Administrator",
-#         "property": "search_fields",
-#         "property_type": "Data",
-#         "value": "barcode"
-#     }
-#  ],
-  
-   'on_setup': 'dynamic.doctor_textile.setup.create_scripts'
+       ]
+    }
 }
