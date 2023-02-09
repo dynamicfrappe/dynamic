8ed1ecaae6b0d305dba40d4126869dfc6ff46dde
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-02-09 13:56:58 +0200

    edit:workspace_seeling_ifi

--- dynamic/cheques/setup.py
+++ dynamic/cheques/setup.py
@@ -1,4 +1,10 @@
-import frappe
 
+from frappe import _
+import frappe
+from dynamic.dynamic_accounts.setup import add_properties
 def install():
-    pass
+    try:
+        add_properties()
+    except Exception as e:
+        frappe.throw(_(str(e)))
+    return
--- dynamic/dynamic_accounts/setup.py
+++ dynamic/dynamic_accounts/setup.py
@@ -3,18 +3,33 @@ import os
 import json
 
 
+from frappe import _
 
 
 
 def install_dynamic_accounts():
     add_properties()
-   
-
-
 
 
 def add_properties():
+	name = "Journal Entry Account-reference_type"
+	if frappe.db.exists("Property Setter",name) :
+		doc = frappe.get_doc("Property Setter",name)
+	else :
+		doc = frappe.new_doc("Property Setter")
+	try:
+		doc.doc_type  = "Journal Entry Account"
+		doc.doctype_or_field = "DocField"
+		doc.field_name = "reference_type"
+		doc.name = name
+		doc.property = "options"
+		doc.property_type = "Text"
+		doc.value = "\nSales Invoice\nPurchase Invoice\nJournal Entry\nSales Order\nPurchase Order\nExpense Claim\nAsset\nLoan\nPayroll Entry\nEmployee Advance\nExchange Rate Revaluation\nInvoice Discounting\nFees\nComparison\nClearance\nTender\nPayroll Month\nCheque\nPay Document\nReceipt Document\nPayment Entry"
+		doc.save()
+	except Exception as e:
+		frappe.throw(_(str(e)))
 	return
+
 	try:
 		name = "Journal Entry Account-reference_type-options"
 		if frappe.db.exists("Property Setter",name) :
