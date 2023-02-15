7cb0949849a2ce689d552f2891389c4adc48a463
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-02-15 16:19:25 +0200

    edit:terra_property

--- dynamic/cheques/setup.py
+++ dynamic/cheques/setup.py
@@ -1,10 +1,10 @@
 
 from frappe import _
 import frappe
-from dynamic.dynamic_accounts.setup import add_properties
+from dynamic.dynamic_accounts.setup import add_properties_journal_auto
 def install():
     try:
-        add_properties()
+        add_properties_journal_auto()
     except Exception as e:
         frappe.throw(_(str(e)))
     pass
--- dynamic/dynamic_accounts/setup.py
+++ dynamic/dynamic_accounts/setup.py
@@ -8,28 +8,11 @@ from frappe import _
 
 
 def install_dynamic_accounts():
-    add_properties()
+	add_properties()
+	add_properties_journal_auto()
 
 
 def add_properties():
-	name = "Journal Entry Account-reference_type"
-	if frappe.db.exists("Property Setter",name) :
-		doc = frappe.get_doc("Property Setter",name)
-	else :
-		doc = frappe.new_doc("Property Setter")
-	try:
-		doc.doc_type  = "Journal Entry Account"
-		doc.doctype_or_field = "DocField"
-		doc.field_name = "reference_type"
-		doc.name = name
-		doc.property = "options"
-		doc.property_type = "Text"
-		doc.value = "\nSales Invoice\nPurchase Invoice\nJournal Entry\nSales Order\nPurchase Order\nExpense Claim\nAsset\nLoan\nPayroll Entry\nEmployee Advance\nExchange Rate Revaluation\nInvoice Discounting\nFees\nComparison\nClearance\nTender\nPayroll Month\nCheque\nPay Document\nReceipt Document\nPayment Entry"
-		doc.save()
-	except Exception as e:
-		frappe.throw(_(str(e)))
-	pass
-
 	try:
 		name = "Journal Entry Account-reference_type-options"
 		if frappe.db.exists("Property Setter",name) :
@@ -65,3 +48,22 @@ def add_properties():
 
 
 
+
+def add_properties_journal_auto():
+	name = "Journal Entry Account-reference_type"
+	if frappe.db.exists("Property Setter",name) :
+		doc = frappe.get_doc("Property Setter",name)
+	else :
+		doc = frappe.new_doc("Property Setter")
+	try:
+		doc.doc_type  = "Journal Entry Account"
+		doc.doctype_or_field = "DocField"
+		doc.field_name = "reference_type"
+		doc.name = name
+		doc.property = "options"
+		doc.property_type = "Text"
+		doc.value = "\nSales Invoice\nPurchase Invoice\nJournal Entry\nSales Order\nPurchase Order\nExpense Claim\nAsset\nLoan\nPayroll Entry\nEmployee Advance\nExchange Rate Revaluation\nInvoice Discounting\nFees\nComparison\nClearance\nTender\nPayroll Month\nCheque\nPay Document\nReceipt Document\nPayment Entry"
+		doc.save()
+	except Exception as e:
+		frappe.throw(_(str(e)))
+	pass
