0a1c6b9a7ca2a82b87fd45dc0141d1667f4dd022
Kazem <kazemaraby@gmail.com>
2023-02-19 15:17:06 +0200

    add company arabic name in dynamic accounts instead vero

--- dynamic/domains/vero.py
+++ dynamic/domains/vero.py
@@ -2,15 +2,15 @@ from __future__ import unicode_literals
 
 
 data = {
-
-    'custom_fields': {
-       "Company":[
-        {
-            "fieldname":"company_arabic_name",
-            "fieldtype":"Data",
-            "insert_after":"company_name",
-            "label":"Company Arabic Name",
-        }
-       ]
-    }
-}
+    'custom_fields': {}
+    # 'custom_fields': {
+    #    "Company":[
+    #     {
+    #         "fieldname":"company_arabic_name",
+    #         "fieldtype":"Data",
+    #         "insert_after":"company_name",
+    #         "label":"Company Arabic Name",
+    #     }
+    #    ]
+    # }
+} 
