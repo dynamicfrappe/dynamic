476780a2bf0049b94a47bb65010e6894704b0ca2
beshoy <beshoyatef31@gmail.com>
2023-02-19 17:10:57 +0200

    add test api

--- dynamic/elevana/api.py
+++ dynamic/elevana/api.py
@@ -1,9 +1,9 @@
 import frappe
 from frappe.model.mapper import get_mapped_doc
 
+from werkzeug.wrappers import Response
 
-
-
+import json
 
 def elevana_lead_before_insert(doc,fun=''):
     set_sales_person(doc)
@@ -76,3 +76,28 @@ def _set_missing_values(source, target):
             # "incentives":"150"
         })
 
+
+        #dynamic.dynamic.elevana.api.get_customer_name
+@frappe.whitelist(allow_guest =1)
+def get_customer_name (*args , **kwargs)  :
+    #get data 
+    data = False
+    respone = Response()
+    try :
+        data =json.loads(frappe.request.data)
+    except Exception as e :
+        frappe.local.response['message'] = f"Error Accourd   {e}"
+        frappe.local.response['http_status_code'] = 400 
+
+    if data :
+        #check phone number 
+        if not data.get("phone"):
+            # frappe.local.response['message'] = "Customer name required"
+            # frappe.local.response['http_status_code'] = 400 
+            respone.data = "error!"
+            return
+        sql = frappe.db.sql(f""" SELECT link_title FROM 
+            `tabDynamic Link` WHERE parent in  (SELECT parent From `tabContact Phone` WHERE phone =  "{data.get('phone')}" ) """ ,as_dict =1)
+        if sql and len(sql) > 0:
+            respone.data = str(sql[-1].get("link_title"))
+            return respone
