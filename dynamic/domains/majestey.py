0896dda4ba42f31a0cf9705725da3091803f04fa
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-03-27 14:14:42 +0200

    edit:feat:majesty

---
+++ dynamic/domains/majestey.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+data = {
+
+    'custom_fields': {
+        'Delivery Note': [
+            {
+                "label": "Voucher Number",
+                "fieldname": "voucher_number",
+                "fieldtype": "Data",
+                "insert_after": "customer",
+            }
+        ],
+
+       
+    },
+    "properties": [
+       
+    ],
+    "property_setters": [
+
+    ],
+    # 'on_setup': 'dynamic.majestey.setup.install_majestey'
+}
