55f1d25a96ad9845831add1a352ab6cd4da5305f
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-03-30 10:04:13 +0200

    edit:css printformat ifi

--- dynamic/terra/report/warehouse_reservation_qty_available/warehouse_reservation_qty_available.py
+++ dynamic/terra/report/warehouse_reservation_qty_available/warehouse_reservation_qty_available.py
@@ -13,10 +13,10 @@ from frappe.utils import (
 )
 
 def execute(filters=None):
-	return PayInvoicePerOrder(filters).run()
+	return WarehouseReservationAvailQty(filters).run()
 
 
-class PayInvoicePerOrder(object):
+class WarehouseReservationAvailQty(object):
 	def __init__(self,filters):
 		self.filters  = frappe._dict(filters or {})
 	def run(self):
@@ -31,40 +31,16 @@ class PayInvoicePerOrder(object):
 
 	def get_transaction(self,filters):
 		conditions = "  1=1 "
-		get_new = self.get_all_data(conditions)
+		get_new = self.get_avail_qty(conditions)
 		return get_new
 
-	def get_all_data(self,conditions):
+	def get_avail_qty(self,conditions):
 		if self.filters.get("from_date"):
 			conditions += " AND `tabPurchase Invoice`.creation >= '%s'"%self.filters.get("from_date")
 		if self.filters.get("to_date"):
 			conditions += " AND `tabPurchase Invoice`.creation <= '%s'"%self.filters.get("to_date")
 		sql_query_new = f"""
-		SELECT DISTINCT `tabPayment Entry Reference`.parent payment_entry
-		,`tabPurchase Invoice Item`.parent purchase_invoice
-		,`tabPurchase Order`.name purchase_order
-		,`tabPayment Entry Reference`.reference_doctype
-		,`tabPayment Entry Reference`.reference_name
-		,`tabPayment Entry Reference`.allocated_amount 
-		,`tabPurchase Order`.grand_total po_order_amount
-		,`tabPurchase Invoice`.grand_total pi_invoice_amount
-		,`tabPayment Entry`.paid_amount
-		,`tabPayment Entry`.unallocated_amount
-		,`tabPayment Entry`.total_allocated_amount
-		FROM `tabPurchase Invoice Item`
-		INNER JOIN `tabPurchase Order`
-		ON `tabPurchase Invoice Item`.purchase_order=`tabPurchase Order`.name
-		INNER JOIN `tabPayment Entry Reference`
-		ON 
-		(`tabPayment Entry Reference`.reference_doctype IN('Purchase Invoice','Purchase Order')
-		AND `tabPayment Entry Reference`.reference_name IN (`tabPurchase Order`.name,`tabPurchase Invoice Item`.parent)
-		)
-		INNER JOIN `tabPurchase Invoice` 
-		ON`tabPurchase Invoice Item`.parent=`tabPurchase Invoice`.name
-		INNER JOIN `tabPayment Entry` 
-		ON `tabPayment Entry`.name=`tabPayment Entry Reference`.parent
-		WHERE  {conditions} AND `tabPayment Entry Reference`.reference_doctype IN('Purchase Invoice','Purchase Order')
-		AND `tabPurchase Invoice`.docstatus<>2 
+		
 		"""
 		sql_data = frappe.db.sql(sql_query_new,as_dict=1)
 
