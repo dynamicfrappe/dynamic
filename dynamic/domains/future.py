70d767a2e75bfb3ebac4c8587d0236141ce0d189
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-05-30 13:27:06 +0300

    edit:grid_view_sales_order

--- dynamic/domains/future.py
+++ dynamic/domains/future.py
@@ -14,13 +14,13 @@ data = {
         "property_type": "Check",
         "value": "0",
         },
-        {
-        "doctype": "Sales Order Item",
-        "doctype_or_field": "DocField",
-        "fieldname": "price_list_rate",
-        "property": "in_list_view",
-        "property_type": "Check",
-        "value": "1",
-        },
+        # {
+        # "doctype": "Sales Order Item",
+        # "doctype_or_field": "DocField",
+        # "fieldname": "price_list_rate",
+        # "property": "in_list_view",
+        # "property_type": "Check",
+        # "value": "1",
+        # },
         ],
 }
