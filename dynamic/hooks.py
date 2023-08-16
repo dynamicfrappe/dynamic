41e23a7ddd69cde8061ee86206b8f3fe01cae05b
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-08-16 11:18:36 +0300

    feat:master_deals_pic

--- dynamic/hooks.py
+++ dynamic/hooks.py
@@ -238,6 +238,9 @@ doc_events = {
     "Stock Ledger Entry":{
         "before_insert":"dynamic.reservation.reservation_api.stock_ledger_entry_before_insert"
     },
+    "File":{
+        "after_insert":"dynamic.master_deals.master_deals_api.deals_after_insert"
+    },
     # "Batch":{
     #     "before_save": "dynamic.api.disable_batch_if_qty_zero"
     # }
