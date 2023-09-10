83cd1557495f3b363e8801bf7301ed850dfe2c2a
Hagar Mossad <hagermossad80@gmail.com>
2023-09-10 14:01:24 +0300

    modify filters in function check open invoice

--- dynamic/controllers/sales_invoice.py
+++ dynamic/controllers/sales_invoice.py
@@ -14,5 +14,5 @@ def validate_items(self):
             frappe.throw(_("Brand of <b>{0}</b> should be <b>{1}</b>").format(item.item_code , self.brand))
 
 def check_open_sales_invoices(self):
-   if frappe.db.exists("Sales Invoice", {"customer": self.customer ,"brand" :self.brand , "status":"Draft"}):
+   if frappe.db.exists("Sales Invoice", {"customer": self.customer ,"brand" :self.brand , "docstatus" : 1 ,"outstanding_amount" : [">" ,1.00] , "name" :["!=", self.name]}):
        frappe.throw(_("There are open sales invoice for <b>{0}</b> with the same brand <b>{1}</b>").format(self.customer, self.brand))
