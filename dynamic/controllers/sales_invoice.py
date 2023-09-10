29c7481668c28625e5c802f908fcff4a7a046c47
Hagar Mossad <hagermossad80@gmail.com>
2023-09-10 10:12:56 +0300

    change name of function

--- dynamic/controllers/sales_invoice.py
+++ dynamic/controllers/sales_invoice.py
@@ -6,14 +6,13 @@ def validate(self , event):
     validate_items(self)
 
 def before_submit(self , event):
-   check_brand_for_customer(self)
-   
+   check_open_sales_invoices(self)
+
 def validate_items(self):
     for item in self.items :
         if item.brand != self.brand:
             frappe.throw(_("Brand of <b>{0}</b> should be <b>{1}</b>").format(item.item_code , self.brand))
 
-
-def check_brand_for_customer(self):
+def check_open_sales_invoices(self):
    if frappe.db.exists("Sales Invoice", {"customer": self.customer ,"brand" :self.brand , "status":"Draft"}):
-       frappe.throw(_("There are open sales invoice for <b>{0}</b> with the same <b>{1}</b>").format(self.customer, self.brand))
+       frappe.throw(_("There are open sales invoice for <b>{0}</b> with the same brand <b>{1}</b>").format(self.customer, self.brand))
