82df4b50fee860bf25e3b750ee7fbc5c53cff6a8
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-10-02 16:53:12 +0300

    feat:add_cst_fields_to_contracting_quotation

--- dynamic/domains/aram.py
+++ dynamic/domains/aram.py
@@ -7,40 +7,7 @@ from frappe import _
 data = {
 
     'custom_fields': {
-        'Quotation':[
-            {
-                "label":_("Project"),
-                "fieldname":"project",
-                "fieldtype":"Link",
-                "options":"Project",
-                "insert_after":"items_section", 
-                "no_copy":1, 
-            },
-            {
-                "label":_(""),
-                "fieldname":"card_items_sect",
-                "fieldtype":"Section Break",
-                "insert_after":"total_net_weight", 
-                "no_copy":1, 
-            },
-            {
-                "label":_("Card Items"),
-                "fieldname":"card_items",
-                "fieldtype":"Table",
-                "options":"Comparison Item Card Stock Item",
-                "insert_after":"card_items_sect", 
-                "no_copy":1, 
-            },
-            {
-                "label":_("Card Services"),
-                "fieldname":"card_services",
-                "fieldtype":"Table",
-                "options":"Comparison Item Card Service Item",
-                "insert_after":"card_items", 
-                "no_copy":1, 
-            },
-            
-        ],
+        
         
     },
       "properties": [
