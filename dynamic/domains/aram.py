e443c0a640a9e100e8d7dece8a6c548adda49fd5
abanoubmounir77 <abanoubmounir07@gmail.com>
2023-10-08 15:43:28 +0300

    hager:print_format _footer

--- dynamic/domains/aram.py
+++ dynamic/domains/aram.py
@@ -7,24 +7,11 @@ from frappe import _
 data = {
 
     'custom_fields': {
-        'Quotation Item':[
-            {
-                "label": "Build",
-                "fieldname": "build",
-                "fieldtype": "Data",
-                "insert_after": "ordered_qty",
-            }
+        'Quotation':[
+            
             
             
         ],
-        'Comparison Item':[
-            {
-                "label": "Build",
-                "fieldname": "build",
-                "fieldtype": "Data",
-                "insert_after": "remaining_purchased_qty",
-            }
-        ]
         
     },
       "properties": [
