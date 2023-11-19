26068f6f0ffccea96ed591733318b6616959c174
Hagar Mossad <hagermossad80@gmail.com>
2023-11-19 16:26:14 +0200

    arange items according idx

--- dynamic/public/js/stock_entry.js
+++ dynamic/public/js/stock_entry.js
@@ -434,7 +434,13 @@ const override_scan_code = erpnext.stock.StockEntry.extend({
 		});
 
 		scan_barcode_field.set_value('');
-		const reversed = this.frm.doc.items.reverse();
+		var reversed = this.frm.doc.items.reverse();
+    let count = 0
+    reversed.forEach((item , index) => {
+      item.idx = count +1;
+      count++;
+    })
+
 		refresh_field("items");
 	},
 	show_scan_message (idx, exist = null) {
