13e0e3f4cec3586462f1e3d7eb9f8d865002a24b
Hagar Mossad <hagermossad80@gmail.com>
2023-12-03 14:09:08 +0200

    add item sales and purchase of item report

--- dynamic/qaswaa/report/item_sales/item_sales.js
+++ dynamic/qaswaa/report/item_sales/item_sales.js
@@ -2,33 +2,42 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["offer price"] = {
+frappe.query_reports["Item sales"] = {
 	"filters": [
 		{
 			fieldname: "from_date",
 			label: __("From Date"),
 			fieldtype: "Date",
-			default: frappe.datetime.get_today(),
+			// default: frappe.datetime.get_today(),
+			reqd : 1
 		},
 		{
 			fieldname: "to_date",
 			label: __("To Date"),
 			fieldtype: "Date",
-			default: frappe.datetime.add_days(frappe.datetime.get_today(),1),
-		},
-		{
-			fieldname: "customer",
-			label: __("Customer"),
-			fieldtype: "Link",
-			options: "Customer" ,
-
+			// default: frappe.datetime.add_days(frappe.datetime.get_today(),1),
+			reqd : 1
 		},
 		{
 			fieldname: "item_code",
 			label: __("Item"),
 			fieldtype: "Link",
 			options: "Item" ,
-			reqd : 1
+			reqd: 1
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer" ,
+			reqd: 1
+		},
+		{
+			fieldname: "item_group",
+			label: __("Item Group"),
+			fieldtype: "Link",
+			options: "Item Group" ,
+			reqd: 1
 		},
 	]
 };
