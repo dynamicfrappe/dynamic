0fe783a0ddc5ab83ab3a52dcb506bb94201245e4
abanoubmounir <abanoubmounir07@gmail.com>
2024-01-02 12:26:01 +0200

    feat:add_validation_warrhouse_weh

--- dynamic/controllers/stock_entry.py
+++ dynamic/controllers/stock_entry.py
@@ -9,6 +9,6 @@ DOMAINS = frappe.get_active_domains()
 
 def before_save_stock_entry(doc, *args, **kwargs):
     if "WEH" in DOMAINS:
-        get_allowed_stoc_use_submit(doc, doc,doc.get("from_warehouse"))
+        get_allowed_stoc_use_submit(doc,doc.get("from_warehouse"))
 
 
