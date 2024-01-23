49c1b86d3ce3d1d1a6b5ce5581fc736365febc37
abanoubmounir <abanoubmounir07@gmail.com>
2024-01-23 10:46:06 +0200

    fix:domain master_deals

--- dynamic/public/js/supplier.js
+++ dynamic/public/js/supplier.js
@@ -3,17 +3,27 @@
 frappe.ui.form.on("Supplier", {
     onload: function(frm){
         frappe.call({
-            method:"dynamic.master_deals.master_deals_api.get_last_doctype",
-            args:{
-                doc_type: frm.doctype
-            },
-            callback:function(r){
-                if (r){
-                    frm.set_value('last_supplier',r.message.name)
-                    frm.refresh_field("last_supplier")
+            method: "dynamic.api.get_active_domains",
+            callback: function (r) {
+                if (r.message && r.message.length) {
+                    if (r.message.includes("Master Deals")) {
+                        frappe.call({
+                            method:"dynamic.master_deals.master_deals_api.get_last_doctype",
+                            args:{
+                                doc_type: frm.doctype
+                            },
+                            callback:function(r){
+                                if (r){
+                                    frm.set_value('last_supplier',r.message.name)
+                                    frm.refresh_field("last_supplier")
+                                }
+                            }
+                        })
+                    }
                 }
             }
         })
+        
     },
     check_url:function(frm){
         if(frm.doc.url){
