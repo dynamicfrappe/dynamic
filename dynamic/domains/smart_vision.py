970073dacdde03c12c550c9ad53ea734e7de3d85
Hagar Mossad <hagermossad80@gmail.com>
2024-02-04 11:12:33 +0200

    add sales invoicew smart vision

--- dynamic/domains/smart_vision.py
+++ dynamic/domains/smart_vision.py
@@ -28,6 +28,36 @@ data = {
                 "insert_after":"customer_name",
             },
         ],
+        'Sales Invoice':[
+            {
+                "label":"End User",
+                "fieldname":"end_user",
+                "fieldtype":"Link",
+                "options":'Customer',
+                "insert_after":"customer",
+            },
+            {
+                "label":"End User",
+                "fieldname":"end_user",
+                "fieldtype":"Link",
+                "options":'Customer',
+                "insert_after":"customer",
+            },
+        ],
+        'Sales Invoice Item':[
+            {
+                "label":"Start Date",
+                "fieldname":"start_data",
+                "fieldtype":"Date",
+                "insert_after":"item_name",
+            },
+            {
+                "label":"End Date",
+                "fieldname":"end_date",
+                "fieldtype":"Date",
+                "insert_after":"start_data",
+            },
+        ],
         
     },
 }
