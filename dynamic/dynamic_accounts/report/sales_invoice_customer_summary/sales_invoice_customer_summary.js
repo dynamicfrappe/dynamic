5259fc66881d1b8a8d3411082478f72c763c2483
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-05-20 13:57:21 +0300

    Adding filters to Sales Invoice Customer Summary report

--- dynamic/dynamic_accounts/report/sales_invoice_customer_summary/sales_invoice_customer_summary.js
+++ dynamic/dynamic_accounts/report/sales_invoice_customer_summary/sales_invoice_customer_summary.js
@@ -5,10 +5,18 @@
 frappe.query_reports["Sales Invoice Customer Summary"] = {
 	"filters": [
 		{
-			"fieldname":"item_code",
-			"label": __("Item"),
-			"fieldtype": "Link",
-			"options": "Item"
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"reqd": 1
 		},
 		{
 			"fieldname":"customer",
@@ -16,6 +24,41 @@ frappe.query_reports["Sales Invoice Customer Summary"] = {
 			"fieldtype": "Link",
 			"options": "Customer"
 		},
-	
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item"
+		},
+		{
+			"fieldname":"warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		},
+		{
+			"fieldname": "invoice",
+			"label": __("Sales Invoice"),
+			"fieldtype": "Link",
+			"options": "Sales Invoice",
+		},
+		{
+			"fieldname": "sales_person",
+			"label": __("Sales Person"),
+			"fieldtype": "Link",
+			"options": "Sales Person",
+		},
+		{
+			"fieldname": "cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "Link",
+			"options": "Cost Center",
+		},
+		{
+			"fieldname": "sales_partner",
+			"label": __("Sales Partner"),
+			"fieldtype": "Link",
+			"options": "Sales Partner",
+		},
 	]
 };
--- dynamic/dynamic_accounts/report/sales_invoice_customer_summary/sales_invoice_customer_summary.py
+++ dynamic/dynamic_accounts/report/sales_invoice_customer_summary/sales_invoice_customer_summary.py
@@ -5,53 +5,74 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
-	columns, data = [], []
-	columns = get_columns(filters or {})
-	data = get_data(filters or {})
-	return columns, data
+    columns, data = [], []
+    columns = get_columns(filters or {})
+    data = get_data(filters or {})
+    return columns, data
 
 
 
 
 def get_data(filters):
-	conditions = get_conditios(filters)
-	sql = f"""
-		SELECT `tabSales Invoice`.name invoice,`tabSales Invoice`.posting_date , `tabSales Invoice Item`.item_code,
-			`tabSales Invoice Item`.qty,`tabSales Invoice Item`.rate ,
-			(`tabSales Invoice Item`.qty * `tabSales Invoice Item`.rate) total_sales,
-			`tabStock Ledger Entry`.valuation_rate cost,
-			(`tabStock Ledger Entry`.valuation_rate * `tabSales Invoice Item`.qty) total_cost,
-			((`tabSales Invoice Item`.qty * `tabSales Invoice Item`.rate) - (`tabStock Ledger Entry`.valuation_rate * `tabSales Invoice Item`.qty))profit,
-			`tabSales Invoice`.customer 
-			,`tabSales Team`.sales_person ,
-			`tabSales Invoice`.territory,
-			`tabSales Invoice Item`.`item_name`, `tabSales Invoice Item`.`item_group`,
-			`tabSales Invoice`.customer_name
-		from `tabSales Invoice`
-		INNER JOIN `tabSales Invoice Item`
-			ON `tabSales Invoice`.name = `tabSales Invoice Item`.parent
-		INNER JOIN `tabStock Ledger Entry`
-			ON `tabStock Ledger Entry`.item_code = `tabSales Invoice Item`.item_code
-		INNER JOIN `tabSales Team`
-		ON `tabSales Team`.parent =  `tabSales Invoice`.name  AND `tabSales Team`.parent = `tabSales Invoice Item`.parent
-		where  `tabSales Invoice`.docstatus = 1 {conditions}
-		group by `tabSales Invoice`.name, `tabSales Invoice Item`.item_code ,`tabSales Invoice`.customer
-		order by `tabSales Invoice`.name desc
-		"""
-	sql = frappe.db.sql(sql,as_dict=1)
-	return sql
+    conditions = get_conditios(filters)
+    sql = f"""
+        SELECT `tabSales Invoice`.name invoice,`tabSales Invoice`.posting_date , `tabSales Invoice Item`.item_code,
+            `tabSales Invoice Item`.qty,`tabSales Invoice Item`.rate ,
+            (`tabSales Invoice Item`.qty * `tabSales Invoice Item`.rate) total_sales,
+            `tabStock Ledger Entry`.valuation_rate cost,
+            (`tabStock Ledger Entry`.valuation_rate * `tabSales Invoice Item`.qty) total_cost,
+            ((`tabSales Invoice Item`.qty * `tabSales Invoice Item`.rate) - (`tabStock Ledger Entry`.valuation_rate * `tabSales Invoice Item`.qty))profit,
+            `tabSales Invoice`.customer 
+            ,`tabSales Team`.sales_person ,
+            `tabSales Invoice`.territory,
+            `tabSales Invoice Item`.`item_name`, `tabSales Invoice Item`.`item_group`,
+            `tabSales Invoice`.customer_name
+        from `tabSales Invoice`
+        INNER JOIN `tabSales Invoice Item`
+            ON `tabSales Invoice`.name = `tabSales Invoice Item`.parent
+        INNER JOIN `tabStock Ledger Entry`
+            ON `tabStock Ledger Entry`.item_code = `tabSales Invoice Item`.item_code
+        INNER JOIN `tabSales Team`
+        ON `tabSales Team`.parent =  `tabSales Invoice`.name  AND `tabSales Team`.parent = `tabSales Invoice Item`.parent
+        where  `tabSales Invoice`.docstatus = 1 {conditions}
+        group by `tabSales Invoice`.name, `tabSales Invoice Item`.item_code ,`tabSales Invoice`.customer
+        order by `tabSales Invoice`.name desc
+        """
+    sql = frappe.db.sql(sql,as_dict=1)
+    return sql
 
 def get_conditios(filters):
-	conditions = ' AND 1=1 '
-	if filters.get('customer'):
-		conditions += " AND `tabSales Invoice`.customer = '%s' "%(filters.get('customer'))
-	if filters.get('item_code'):
-		conditions += " AND `tabSales Invoice Item`.item_code = '%s' "%(filters.get('item_code'))
-	return conditions
+    conditions = ' AND 1=1 '
+    if filters.get('from_date') and filters.get('to_date'):
+        conditions += f" and date(`tabSales Invoice`.posting_date) BETWEEN date('{filters.get('from_date')}') AND date('{filters.get('to_date')}')"
+    else:
+        if filters.get('from_date'):
+            conditions += f" and date(`tabSales Invoice`.posting_date) >= date('{filters.get('from_date')}')"
+        if filters.get('to_date'):
+            conditions += f" and date(`tabSales Invoice`.posting_date) <= date('{filters.get('to_date')}')"
+    if filters.get('customer'):
+        conditions += " AND `tabSales Invoice`.customer = '%s' "%(filters.get('customer'))
+    if filters.get('item_code'):
+        conditions += " AND `tabSales Invoice Item`.item_code = '%s' "%(filters.get('item_code'))
+    if filters.get('warehouse'):
+        conditions += " AND `tabSales Invoice`.set_warehouse = '%s' "%(filters.get('warehouse'))
+    if filters.get('invoice'):
+        conditions += " AND `tabSales Invoice`.name = '%s' "%(filters.get('invoice'))
+    if filters.get('sales_person'):
+        conditions += " AND `tabSales Team`.sales_person = '%s' "%(filters.get('sales_person'))
+    if filters.get('cost_center'):
+        conditions += " AND `tabSales Invoice`.cost_center = '%s' "%(filters.get('cost_center'))
+    if filters.get('sales_partner'):
+        conditions += " AND `tabSales Invoice`.sales_partner = '%s' "%(filters.get('sales_partner'))
+    
+    
+
+    
+    return conditions
 
 
 def get_columns(filters):
-	columns = [
+    columns = [
         {
             "label": _("Invoice"),
             "fieldname": "invoice",
@@ -63,7 +84,7 @@ def get_columns(filters):
             "label": _("Customer"),
             "fieldname": "customer",
             "fieldtype": "Link",
-			"options": "Customer",
+            "options": "Customer",
             "width": 120
         },
         {
@@ -77,7 +98,7 @@ def get_columns(filters):
             "label": _("Item Group"),
             "fieldname": "item_group",
             "fieldtype": "Link",
-			"options": "Item Group",
+            "options": "Item Group",
             "width": 120
         },
         {
@@ -92,7 +113,7 @@ def get_columns(filters):
             "fieldtype": "Float",
             "width": 120
         },
-		 {
+         {
             "label": _("Rate"),
             "fieldname": "rate",
             "fieldtype": "Float",
@@ -110,27 +131,26 @@ def get_columns(filters):
             "fieldtype": "Float",
             "width": 120
         },
-		{
+        {
             "label": _("Total Cost"),
             "fieldname": "total_cost",
             "fieldtype": "Float",
             "width": 120
         },
-		{
+        {
             "label": _("Profit"),
             "fieldname": "profit",
             "fieldtype": "Float",
             "width": 120
         },
-
-		{
+        {
             "label": _("Sales Person"),
             "fieldname": "sales_person",
             "fieldtype": "Link",
-			"options": "Sales Person",
+            "options": "Sales Person",
             "width": 120
         },
-		{
+        {
             "label": _("Territory"),
             "fieldname": "territory",
             "fieldtype": "Data",
@@ -138,5 +158,5 @@ def get_columns(filters):
         },
 
     ]
-	return columns
-		
+    return columns
+        
