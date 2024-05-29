b8bbd72199e9c64362b24e489e30dbdae4bfa7fb
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-05-29 17:10:04 +0300

    Adding report summary to 2 reports

--- dynamic/qaswaa/report/compare_prices_of_sales_order_with_price_list/compare_prices_of_sales_order_with_price_list.py
+++ dynamic/qaswaa/report/compare_prices_of_sales_order_with_price_list/compare_prices_of_sales_order_with_price_list.py
@@ -5,8 +5,52 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
-	columns, data = get_columns(), get_date(filters)
-	return columns, data
+    columns, data = get_columns(), get_date(filters)
+    summary = get_report_summary(data)
+    return columns, data, None, None, summary
+
+def get_report_summary(data):
+    if not data:
+        return None
+
+    total_sales_rate = round(sum([float(row.get("sales_rate") or 0) for  row in data]),2)
+    total_of_total_cost = round(sum([float(row.get("total_cost") or 0) for  row in data]),2)
+    total_difference  = round(sum([float(row.get("difference") or 0) for  row in data]),2)
+    ratio = calc_ratio(total_of_total_cost, total_difference)
+    return[
+        {
+            'value' : total_sales_rate,
+            'indicator' : 'Blue',
+            'label' : _('Total Sales Rate'),
+            'datatype' : 'Currency',
+        },
+        {
+            'value' : total_of_total_cost,
+            'indicator' : 'Blue',
+            'label' :  _('Total Of Average Cost'),
+            'datatype' : 'Currency',
+        },
+        {
+            'value' : total_difference,
+            'indicator' : 'Blue',
+            'label' : _('Total Difference'),
+            'datatype' : 'Currency',
+        },
+        {
+            'value' : ratio,
+            'indicator' : 'Blue',
+            'label' : _('Ratio'),
+            'datatype' : 'Percent',
+        }
+    ]
+
+def calc_ratio(total_of_total_cost, total_difference):
+    ratio = 0.0
+    if total_of_total_cost:
+        ratio  = (total_difference/total_of_total_cost) *100
+    else:
+        frappe.msgprint("Total Of Average Cost is Zero. Can't calculate the ratio.")
+    return ratio
 
 def get_date(filters):
     conditions = " 1=1 "
@@ -83,108 +127,108 @@ def get_date(filters):
 
 
 def get_columns():
-	return[
-		{
-			"fieldname": "sales_order_name",
-			"label": _("Sales Order"),
-			"fieldtype": "Link",
-			"options": "Sales Order",
-			"width": 200,
-		},
-		{
-			"fieldname": "item_code",
-			"label": _("Item code"),
-			"fieldtype": "Data",
-			"width": 200,
-		},
-		{
-			"fieldname": "item_name",
-			"label": _("Item Name"),
-			"fieldtype": "Data",
-			"width": 300,
-		},
-		{
-			"fieldname": "qty",
-			"label": _("Qty"),
-			"fieldtype": "Data",
-			"width": 50,
-		},
-		{
-			"fieldname": "sales_rate",
-			"label": "Sales Rate",
-			"fieldtype": "Currency",
-			"options": "currency",
-			"width": 100,
-		},
-		{
-			"fieldname": "purchase_rate",
-			"label": "Rate for Price List",
-			"fieldtype": "Currency",
-			"options": "currency",
-			"width": 100,
-		},
-		{
-			"fieldname": "total_cost",
-			"label": "Total Cost",
-			"fieldtype": "Currency",
-			"options": "currency",
-			"width": 100,
-		},
-		{
-			"fieldname": "difference",
-			"label": "Difference",
-			"fieldtype": "Currency",
-			"options": "currency",
-			"width": 100,
-		},
-	    {
-			"fieldname": "total_difference",
-			"label": "Total Difference",
-			"fieldtype": "Currency",
-			"options": "currency",
-			"width": 100,
-		},
-		{
-			"fieldname": "difference_percentage",
-			"label": _("Difference Percentage"),
-			"fieldtype": "Percent",
-			"width": 100,
-		}
-		# {
-		# 	"fieldname": "sales_rate",
-		# 	"label": "Sales Rate",
-		# 	"fieldtype": "Currency",
-		# 	"options": "currency",
-		# 	"width": 100,
-		# },
-		# {
-		# 	"fieldname": "total_sales",
-		# 	"label": "Total Sales",
-		# 	"fieldtype": "Currency",
-		# 	"options": "currency",
-		# 	"width": 100,
-		# },
-		# {
-		# 	"fieldname": "differance",
-		# 	"label": "Differance",
-		# 	"fieldtype": "Currency",
-		# 	"options": "currency",
-		# 	"width": 100,
-		# },
-		# {
-		# 	"fieldname": "total_difference",
-		# 	"label": "Total Difference",
-		# 	"fieldtype": "Currency",
-		# 	"options": "currency",
-		# 	"width": 100,
-		# },
-		# {
-		# 	"fieldname": "differance_percentage",
-		# 	"label": _("Differance Percentage"),
-		# 	"fieldtype": "Data",
-		# 	"width": 100,
-		# }
-	]
+    return[
+        {
+            "fieldname": "sales_order_name",
+            "label": _("Sales Order"),
+            "fieldtype": "Link",
+            "options": "Sales Order",
+            "width": 200,
+        },
+        {
+            "fieldname": "item_code",
+            "label": _("Item code"),
+            "fieldtype": "Data",
+            "width": 200,
+        },
+        {
+            "fieldname": "item_name",
+            "label": _("Item Name"),
+            "fieldtype": "Data",
+            "width": 300,
+        },
+        {
+            "fieldname": "qty",
+            "label": _("Qty"),
+            "fieldtype": "Data",
+            "width": 50,
+        },
+        {
+            "fieldname": "sales_rate",
+            "label": "Sales Rate",
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 100,
+        },
+        {
+            "fieldname": "purchase_rate",
+            "label": "Rate for Price List",
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 100,
+        },
+        {
+            "fieldname": "total_cost",
+            "label": "Total Cost",
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 100,
+        },
+        {
+            "fieldname": "difference",
+            "label": "Difference",
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 100,
+        },
+        {
+            "fieldname": "total_difference",
+            "label": "Total Difference",
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 100,
+        },
+        {
+            "fieldname": "difference_percentage",
+            "label": _("Difference Percentage"),
+            "fieldtype": "Percent",
+            "width": 100,
+        }
+        # {
+        # 	"fieldname": "sales_rate",
+        # 	"label": "Sales Rate",
+        # 	"fieldtype": "Currency",
+        # 	"options": "currency",
+        # 	"width": 100,
+        # },
+        # {
+        # 	"fieldname": "total_sales",
+        # 	"label": "Total Sales",
+        # 	"fieldtype": "Currency",
+        # 	"options": "currency",
+        # 	"width": 100,
+        # },
+        # {
+        # 	"fieldname": "differance",
+        # 	"label": "Differance",
+        # 	"fieldtype": "Currency",
+        # 	"options": "currency",
+        # 	"width": 100,
+        # },
+        # {
+        # 	"fieldname": "total_difference",
+        # 	"label": "Total Difference",
+        # 	"fieldtype": "Currency",
+        # 	"options": "currency",
+        # 	"width": 100,
+        # },
+        # {
+        # 	"fieldname": "differance_percentage",
+        # 	"label": _("Differance Percentage"),
+        # 	"fieldtype": "Data",
+        # 	"width": 100,
+        # }
+    ]
 
 
 
