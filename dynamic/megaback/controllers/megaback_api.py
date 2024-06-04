50e352040da0af45c2e6e4e12ef59ce761ae591b
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-06-04 17:41:08 +0300

    Adding Gross Weight and Total Gross Weight custom fields.

--- dynamic/megaback/controllers/megaback_api.py
+++ dynamic/megaback/controllers/megaback_api.py
@@ -9,9 +9,16 @@ def set_volumes(self, *args, **kwargs):
         for item in self.items:
             item_doctype = frappe.get_doc("Item", item.item_code)
             item.volume =  item_doctype.volume
-            
             if item.qty and item.volume:
                 item.total_volume = float(item.qty) * float(item.volume)
                 total += float(item.total_volume) or 0
 
         self.items_total_volume = total
+
+def set_total_gross_weight(self, *args, **kwargs):
+    if "Megaback" in DOMAINS:
+        for item in self.items:
+            item_doctype = frappe.get_doc("Item", item.item_code)
+            item.gross_weight =  item_doctype.gross_weight
+            if item.qty and item.gross_weight:
+                item.total_gross_weight = float(item.qty) * float(item.gross_weight)
