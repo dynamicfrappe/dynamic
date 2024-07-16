bc93f07561c22e63d22d79435ef210550a9b5262
Baio <mo.yassin344@gmail.com>
2024-07-16 13:16:07 +0300

    edit

--- dynamic/qaswaa/controllers/purchase_order.py
+++ dynamic/qaswaa/controllers/purchase_order.py
@@ -1,11 +1,13 @@
 import frappe
 from frappe import _
+DOMAINS = frappe.get_active_domains()
 
 @frappe.whitelist()
 def validate(doc, method):
-    item_discount_rate = doc.item_discount_rate
-    for item in doc.items:
-        item.discount_percentage = item_discount_rate
-        item.discount_amount = item.price_list_rate * (item_discount_rate / 100)
-        item.rate = item.price_list_rate - item.discount_amount
-        item.amount = item.rate * item.qty
+    if "Qaswaa" in DOMAINS:
+        item_discount_rate = doc.item_discount_rate
+        for item in doc.items:
+            item.discount_percentage = item_discount_rate
+            item.discount_amount = item.price_list_rate * (item_discount_rate / 100)
+            item.rate = item.price_list_rate - item.discount_amount
+            item.amount = item.rate * item.qty
