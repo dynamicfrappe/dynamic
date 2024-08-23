ab8f2fff642866106c424340eefd55d3dec8a324
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-08-23 04:36:54 +0300

    fix

--- dynamic/controllers/custom_item.py
+++ dynamic/controllers/custom_item.py
@@ -127,5 +127,5 @@ def get_users_warehouse(warehouse):
 
 @frappe.whitelist()
 def check_docstatus(doc):
-	doc_obj = frappe.get_doc("Stock Entry" , doc.name)
+	doc_obj = frappe.get_doc("Stock Entry" , doc)
 	return doc_obj.docstatus
