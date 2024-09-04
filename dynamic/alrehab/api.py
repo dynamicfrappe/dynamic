33e490024ddb2e384a1c468eb298f3434c2b1cb2
Esraa <esraaelzoghby11@gmail.com>
2024-09-04 15:21:31 +0300

    rehab fixing

--- dynamic/alrehab/api.py
+++ dynamic/alrehab/api.py
@@ -52,7 +52,7 @@ def create_deferred_revenue_entry(doc_name):
         # doc.db_set("docstatus", 1, commit=True)
         company = frappe.get_doc('Company', doc.company)
 
-        deferred_revenue_amount = doc.deferred_revenue_amount
+        deferred_revenue_amount = invoice.deferred_revenue_amount
         if deferred_revenue_amount <= 0 :
             frappe.throw(_("Deferred Revenue Amount must be greater than zero."))
 
