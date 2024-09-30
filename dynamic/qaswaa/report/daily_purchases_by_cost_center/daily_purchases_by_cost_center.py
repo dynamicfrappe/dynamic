b1a6a1552fe4153ac65690e250ec1f864c8db2b3
fr3ooon1 <fero>
2024-09-30 16:11:19 +0300

    fix qaswaa error

--- dynamic/qaswaa/report/daily_purchases_by_cost_center/daily_purchases_by_cost_center.py
+++ dynamic/qaswaa/report/daily_purchases_by_cost_center/daily_purchases_by_cost_center.py
@@ -12,63 +12,63 @@ def execute(filters=None):
 
 
 def get_data(filters=None):
-    result = []
-    conditions = []
+	result = []
+	conditions = []
 
-    if filters and filters.get("supplier"):
-        conditions.append(['supplier', '=', filters.get("supplier")])
+	if filters and filters.get("supplier"):
+		conditions.append(['supplier', '=', filters.get("supplier")])
 	if filters and filters.get("supplier_name"):
-        conditions.append(['supplier_name', '=', filters.get("supplier_name")])
+		conditions.append(['supplier_name', '=', filters.get("supplier_name")])
 	if filters and filters.get("supplier_group"):
-        suppliers_in_group = frappe.get_all("Supplier", filters={"supplier_group": filters.get("supplier_group")}, fields=["name"])
-        supplier_names = [supplier.name for supplier in suppliers_in_group]
-        if supplier_names:
-            conditions.append(['supplier', 'in', supplier_names])
-        else:
-            return []
-    if filters and filters.get("set_warehouse"):
-        conditions.append(['set_warehouse', '=', filters.get("set_warehouse")])
-    if filters and filters.get("cost_center"):
-        conditions.append(['cost_center', '=', filters.get("cost_center")])        
-    if filters and filters.get("period_start_date"):
-        conditions.append(('posting_date', '>=', filters.get("period_start_date")))
-    if filters and filters.get("period_end_date"):
-        conditions.append(('posting_date', '<=', filters.get("period_end_date")))
-    if filters and filters.get("is_return") and filters.get("is_return") == 1:
-        conditions.append(['status', '=', 'Return'])
+		suppliers_in_group = frappe.get_all("Supplier", filters={"supplier_group": filters.get("supplier_group")}, fields=["name"])
+		supplier_names = [supplier.name for supplier in suppliers_in_group]
+		if supplier_names:
+			conditions.append(['supplier', 'in', supplier_names])
+		else:
+			return []
+	if filters and filters.get("set_warehouse"):
+		conditions.append(['set_warehouse', '=', filters.get("set_warehouse")])
+	if filters and filters.get("cost_center"):
+		conditions.append(['cost_center', '=', filters.get("cost_center")])        
+	if filters and filters.get("period_start_date"):
+		conditions.append(('posting_date', '>=', filters.get("period_start_date")))
+	if filters and filters.get("period_end_date"):
+		conditions.append(('posting_date', '<=', filters.get("period_end_date")))
+	if filters and filters.get("is_return") and filters.get("is_return") == 1:
+		conditions.append(['status', '=', 'Return'])
 	if filters and filters.get("status"):
-        conditions.append(['status', '=', filters.get("status") ])
-    conditions.append(['docstatus', '!=', 2])
+		conditions.append(['status', '=', filters.get("status") ])
+	conditions.append(['docstatus', '!=', 2])
 
-    purchases_invoices = frappe.get_all("Purchase Invoice", fields=["posting_date", "name", "set_warehouse", "supplier", "status",
-                                                                    "net_total", "base_total_taxes_and_charges",
-                                                                    "base_grand_total", "total_advance", "is_return", "return_against",
-                                                                    "outstanding_amount"],
-                                        filters=conditions)
+	purchases_invoices = frappe.get_all("Purchase Invoice", fields=["posting_date", "name", "set_warehouse", "supplier", "status",
+																	"net_total", "base_total_taxes_and_charges",
+																	"base_grand_total", "total_advance", "is_return", "return_against",
+																	"outstanding_amount"],
+										filters=conditions)
 
-    for doc in purchases_invoices:
-        num = frappe.db.get_value("Purchase Invoice", {"is_return": 1, "return_against": doc.name}, 'base_grand_total') or 0
+	for doc in purchases_invoices:
+		num = frappe.db.get_value("Purchase Invoice", {"is_return": 1, "return_against": doc.name}, 'base_grand_total') or 0
 
-        total_advance = frappe.db.get_value("Payment Entry Reference",
-                                             {"reference_name": doc.name, "reference_doctype": "Purchase Invoice"},
-                                             "allocated_amount") or 0
+		total_advance = frappe.db.get_value("Payment Entry Reference",
+											 {"reference_name": doc.name, "reference_doctype": "Purchase Invoice"},
+											 "allocated_amount") or 0
 
-        temp = {}
-        temp['posting_date'] = doc.posting_date
-        temp['name'] = doc.name
-        temp['warehouse'] = doc.set_warehouse
-        temp['supplier'] = doc.supplier
-        temp['net_total'] = doc.net_total
-        temp['base_total_taxes_and_charges'] = doc.base_total_taxes_and_charges
-        temp['base_grand_total'] = doc.base_grand_total
-        temp['total_advance'] = total_advance
-        temp['refund'] = num if num else 0
-        temp['unpaid'] = doc.outstanding_amount
-        temp['return_agent'] = doc.return_against
+		temp = {}
+		temp['posting_date'] = doc.posting_date
+		temp['name'] = doc.name
+		temp['warehouse'] = doc.set_warehouse
+		temp['supplier'] = doc.supplier
+		temp['net_total'] = doc.net_total
+		temp['base_total_taxes_and_charges'] = doc.base_total_taxes_and_charges
+		temp['base_grand_total'] = doc.base_grand_total
+		temp['total_advance'] = total_advance
+		temp['refund'] = num if num else 0
+		temp['unpaid'] = doc.outstanding_amount
+		temp['return_agent'] = doc.return_against
 
-        result.append(temp)
+		result.append(temp)
 
-    return result
+	return result
 
 def get_columns(filters):
 	columns = [
@@ -86,7 +86,7 @@ def get_columns(filters):
 			"options": "Purchase Invoice",
 			"width": 200,
 		},
-        {
+		{
 			"fieldname": "return_agent",
 			"label": _("Return Agent"),
 			"fieldtype": "Data",
