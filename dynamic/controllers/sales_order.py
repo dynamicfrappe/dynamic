d19decc462b5ecd0b9878cf87331843b87b43fd7
fr3ooon1 <fero>
2024-10-01 18:57:48 +0300

    fix

--- dynamic/controllers/sales_order.py
+++ dynamic/controllers/sales_order.py
@@ -93,17 +93,6 @@ def transfer_items(self , *args, **kwargs):
     # doc.insert()        
 
     
-
-
-
-
-# def on_submit(self , event):
-#     if "Stock Reservation" in Domains:
-#         creation_of_reseration(self , event)
-    
-
-
-    
 def reserve_in_warehouse(self, *args, **kwargs):
     warehouse = frappe.db.get_single_value("Stock Settings" , "warehouse")
     if warehouse:
@@ -111,28 +100,6 @@ def reserve_in_warehouse(self, *args, **kwargs):
         items = self.get("items")
         for item in items:
             item.warehouse = warehouse
-
-
-
-
-def get_validation(self , *args, **kwargs):
-    if frappe.db.get_single_value("Stock Settings" , "allow_partial_reservation"):
-        items = self.get("items")
-        for item in items:
-            item_code = item.item_code
-            qty = float(item.qty_to_reserve or 0)
-            # uom = item.uom
-            warehouse = item.warehouse
-            bin_qty = frappe.db.get_value("Bin" , filters={"item_code":item_code, "warehouse":warehouse} , fieldname = 'actual_qty')
-            # reservation_qty = frappe.db.get_value("Stock Reservation Entry" , filters={"item_code":item_code, "warehouse":warehouse} , fieldname = 'available_qty_to_reserve')
-            reservation_qty = get_all_qty_reserved(item.item_code , item.warehouse)
-            total_qty = float(bin_qty or 0 ) - float(reservation_qty if reservation_qty else 0)
-            if qty > total_qty:
-                wanted_qty = float(qty) - float(total_qty)
-                msg = f"""
-                    Item Name {bold(item.item_code)} Need Qty {bold(wanted_qty)}<br>
-                    """
-                frappe.throw(msg)
 
 
 def creation_of_reseration(self , *args , **kargs):
@@ -176,34 +143,14 @@ def convertor( item_code , uom ):
     return conversion_factor
 
 
-
-def get_all_qty_reserved (item_code, warehouse):
-    item_reserved = frappe.db.sql("""
-    SELECT
-        SUM(`reserved_qty`) as reserved_qty
-    FROM 
-        `tabStock Reservation Entry`
-    WHERE
-        (`status` = 'Partially Reserved' OR `status` = 'Reserved' OR `status` = 'Partially Delivered') 
-        AND `item_code` = %s 
-        AND `warehouse` = %s
-    """, (item_code, warehouse), as_dict=1)
-    qty_reserved = item_reserved[0]['reserved_qty']
-    return qty_reserved
-
-
-
 def get_validation(self , *args, **kwargs):
     if frappe.db.get_single_value("Stock Settings" , "allow_partial_reservation"):
         items = self.get("items")
         for item in items:
             item_code = item.item_code
             qty = item.qty_to_reserve
-            # uom = item.uom
             warehouse = item.warehouse
-            bin_qty = frappe.db.get_value("Bin" , filters={"item_code":item_code, "warehouse":warehouse} , fieldname = 'actual_qty')
-            reservation_qty = frappe.db.get_value("Stock Reservation Entry" , filters={"item_code":item_code, "warehouse":warehouse} , fieldname = 'reserved_qty')
-            total_qty = float(bin_qty or 0) - float(reservation_qty or 0)
+            total_qty = get_all_qty_reserved (item_code, warehouse)
             if qty > total_qty:
                 wanted_qty = float(qty) - float(total_qty)
                 msg = f"""
@@ -211,17 +158,6 @@ def get_validation(self , *args, **kwargs):
                     """
                 frappe.throw(msg)
 
-
-                
-
-def convertor( item_code , uom ):
-    item = frappe.get_doc("Item" , item_code)
-    conversion_factor = 1
-    uoms = item.get("uoms")
-    for i in uoms:
-        if uom == i.uom:
-            conversion_factor = i.conversion_factor
-    return conversion_factor
 
 
 
