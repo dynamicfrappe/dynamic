848ed0d7194ba7f2794b67bd2726475a48e514b0
hagar <hagarmossad80@gmail.com>
2024-10-15 10:27:12 +0300

    add category after sales person

--- dynamic/domains/logistics.py
+++ dynamic/domains/logistics.py
@@ -142,7 +142,7 @@ data = {
                 "label": _("Interested category"),
                 "fieldname": "interested_category",
                 "fieldtype": "Select",
-                "insert_after": "title",
+                "insert_after": "sales_person",
                 "options":'''HSG Bending machine\n3D Printer\nCutter Plotter\nforex sheets\nGrando printer\nHans Laser\nHSG fiberlaser\nindoor power supply\nled modules\nled neon strips\nled neon strips+RGB\nled strips\nled strips (zigzag)\nneon led wire\nrainproof power supply\nstainless steel Letter Coil\nUV\nVacuum Forming Machine\nInks\nHARSLE Press break machine\nIcrelic bending machine\nIcrelic bending machine\nGalvo head machine\nDouble color\nPrinter\nPrinter 160 CM\nCO2 Welding machine\nPlasma machine\nRouter machine\nRouter machine DIACAM\nFlat bed\nWelding machine\nJewelry welding machine\nJewelry welding machine DADO\nLetter bending machine\nLetter bending machine HARFMAK\nLaser CO2 machine\nAll machines\nAdvertising staff\nHARSLE Shearing Machine\n8*1 Punching Machine\nUnavilable product\nMoba machine\nAdhesive tape\nPrinter\nCutter Plotter\nMaterials\nMaintainance\nFiber Marker Machine\nFiber Cutting Machine'''
             },
             {
