3c998eebd6b9c82caa038961b1bb68e9fd5a52b2
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-11-24 13:30:26 +0200

    update

--- dynamic/qaswaa/controllers/item.py
+++ dynamic/qaswaa/controllers/item.py
@@ -9,12 +9,13 @@ DOMAINS = frappe.get_active_domains()
 def after_insert(self , method):
     if "Item Integration" in DOMAINS:
         stock_settings = frappe.get_doc("Stock Settings")
-        if stock_settings.enable_item_integration == 1:
+        if stock_settings.enable_item_itegration == 1:
             url = stock_settings.url_item_integration
             token = stock_settings.token_item_integration
             
             payload = json.dumps({
                 "doctype": "Item",
+                "name":self.name,
                 "item_name": self.item_name,
                 "item_group": self.item_group,
                 "stock_uom": self.stock_uom,
@@ -26,8 +27,8 @@ def after_insert(self , method):
                 'Content-Type': 'application/json',
             }
             response = requests.request("POST", url, headers=headers, data=payload)
-
-            frappe.msgprint("Item Created")
+            if response.status_code == 200:
+                frappe.msgprint("Item Created")
 
 
 
