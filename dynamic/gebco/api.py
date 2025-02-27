89cd7ffa2eef4c79d14d5d154bb3ffa10c10a02a
fr3ooon1 <mohamed.essam68.me@gmail.com>
2025-02-27 15:27:24 +0200

    update

--- dynamic/gebco/api.py
+++ dynamic/gebco/api.py
@@ -166,8 +166,8 @@ def minus_delivery_qty_from_reservation(doc,*args,**kwargs):
     #1-qty deliverd from delivery note
     if not doc.is_return:
         for row in doc.items:
-            reserv_data = frappe.db.get_value('Sales Order Item',{'name':row.name},['reservation','item_purchase_order','item_warehouse'],as_dict=1)
-            reserv_doc = frappe.get_doc('Reservation',reserv_data.get('reservation'))
+            reserv_data = frappe.db.get_value('Sales Order Item',{'item_code':row.item_code,'parent':row.against_sales_order , "qty":row.qty},['reservation','item_purchase_order','item_warehouse'],as_dict=1)
+            reserv_doc = frappe.get_doc('Reservation',reserv_data['reservation'])
             item = frappe.get_doc('Reservation Warehouse',reserv_doc.warehouse[0].name)
             if(item.reserved_qty < row.qty):
                 frappe.throw(f'Not Enough Reservation qty for item {row.item_code} for reservation {reserv_data["reservation"]} avail reserved qty {item.reserved_qty}')
