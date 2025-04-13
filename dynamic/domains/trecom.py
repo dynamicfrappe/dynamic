b0e0aaa16529389cc595b0686aee94f050f858c5
fr3ooon1 <mohamed.essam68.me@gmail.com>
2025-04-13 14:10:29 +0200

    update

--- dynamic/domains/trecom.py
+++ dynamic/domains/trecom.py
@@ -1,4 +1,5 @@
-
+import frappe
+from frappe import _
 
 
 
@@ -8,6 +9,26 @@ from __future__ import unicode_literals
 data = {
 
     'custom_fields': {
+        'Expense Claim':[
+            {
+                "label":_("Payment Methods"),
+                "fieldname":"payment_methods",
+                "fieldtype":"Data",
+                "insert_after":"approval_status",
+            },  
+            {
+                "label":_("Beneficiary Name"),
+                "fieldname":"beneficiary_name",
+                "fieldtype":"Data",
+                "insert_after":"payment_methods"
+            },
+            {
+                "label":_("Choose Currencies"),
+                "fieldname":"choose_currencies",
+                "fieldtype":"Data",
+                "insert_after":"beneficiary_name", 
+            },
+        ],    
     },
       "properties": [
         # Sales Invoice Item
