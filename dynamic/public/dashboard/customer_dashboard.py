8d2ecfea470873374f3918b63c02d82a4f213ead
Moustafa Mahmoud <moustafa.mahmoud.n99@gmail.com>
2025-04-30 13:50:19 +0300

    Refactor

--- dynamic/public/dashboard/customer_dashboard.py
+++ dynamic/public/dashboard/customer_dashboard.py
@@ -12,49 +12,9 @@ def get_data(data={}):
         data["transactions"].append(
             {"label": _("Actions"), "items": ["Appointment", "Actions"]}
         )
+
     if "True lease" in DOMAINS:
         data["non_standard_fieldnames"].update({"Contract": "party_name"})
         print(data)
         data["transactions"].append({"label": _("Contracts"), "items": ["Contract"]})
     return data
-
-
-def get_dasssta():
-    return {
-        "fieldname": "name",
-        "non_standard_fieldnames": {
-            "Delivery Note": "against_sales_invoice",
-            "Journal Entry": "reference_name",
-            "Payment Entry": "reference_name",
-            "Payment Request": "reference_name",
-            "Sales Invoice": "return_against",
-            "Auto Repeat": "reference_document",
-            "Purchase Invoice": "inter_company_invoice_reference",
-        },
-        "internal_links": {
-            "Sales Order": ["items", "sales_order"],
-            "Timesheet": ["timesheets", "time_sheet"],
-        },
-        "internal_and_external_links": {
-            "Delivery Note": ["items", "delivery_note"],
-        },
-        "transactions": [
-            {
-                "label": _("Payment"),
-                "items": [
-                    "Payment Entry",
-                    "Payment Request",
-                    "Journal Entry",
-                    "Invoice Discounting",
-                    "Dunning",
-                ],
-            },
-            {
-                "label": _("Reference"),
-                "items": ["Timesheet", "Delivery Note", "Sales Order"],
-            },
-            {"label": _("Returns"), "items": ["Sales Invoice"]},
-            {"label": _("Subscription"), "items": ["Auto Repeat"]},
-            {"label": _("Internal Transfers"), "items": ["Purchase Invoice"]},
-        ],
-    }
