85935aad76af3f4afb0d9775ede5d0c1589bb10a
Moustafa Mahmoud <moustafa.mahmoud.n99@gmail.com>
2025-05-27 17:45:59 +0300

    Fix: ISS-2025-00224 Connections in Purchase Invoice can now see the connected Purchase Receipts and Purchase Orders connected to it.

--- dynamic/public/dashboard/purchase_invoice_dashboard.py
+++ dynamic/public/dashboard/purchase_invoice_dashboard.py
@@ -13,4 +13,18 @@ def get_data(data={}):
                 'items': ['Cheque']
             }
         )
+        
+    if "Dynamic Accounts" in DOMAINS:
+        data["non_standard_fieldnames"].update({"Purchase Receipt": "purchase_invoice"})
+        data["non_standard_fieldnames"].update({"Purchase Order": "purchase_order"})
+
+        if "internal_links" in data:
+            data["internal_and_external_links"] = data["internal_links"].copy()
+
+            if "Purchase Receipt" in data["internal_links"]:
+                del data["internal_links"]["Purchase Receipt"]
+
+            if "Purchase Order" in data["internal_links"]:
+                del data["internal_links"]["Purchase Order"]
+                
     return data
